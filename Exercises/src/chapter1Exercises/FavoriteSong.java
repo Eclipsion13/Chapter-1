5505802641658e378b7df481abf00bc1fc473d01
Jason Wells <eclipsion013@outlook.com>
2018-08-28 08:33:54 -0500

    Actually did it

---
+++ Exercises/src/chapter1Exercises/FavoriteSong.java
@@ -0,0 +1,14 @@
+package chapter1Exercises;
+
+public class FavoriteSong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("[Insert 1st Song Lyric Here]");
+		System.out.println("[Insert 2nd Song Lyric Here]");
+		System.out.println("[Insert 3rd Song Lyric Here]");
+		System.out.println("[Insert 4th Song Lyric Here]");
+
+	}
+
+}
---
+++ Exercises/src/chapter1Exercises/MovieQuote.java
@@ -0,0 +1,11 @@
+package chapter1Exercises;
+
+public class MovieQuote {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("You probably should have stolen more of the book, because the warnings come after the spells.");
+
+	}
+
+}
